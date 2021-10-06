4e524d6b5044128a32fecf2763319187dc08b1fe
Matti Wilhelmi <Matti.Wilhelmi@hbt.de>
2021-10-06 11:06:49 +0200

    Increased Thread.sleep

--- src/main/java/demo/project/gradle/Callee.java
+++ src/main/java/demo/project/gradle/Callee.java
@@ -8,7 +8,7 @@ public class Callee {
 
     private void innerMethod() {
         try {
-            Thread.sleep(1);
+            Thread.sleep(20);
         } catch (final InterruptedException e) {
             e.printStackTrace();
         }
